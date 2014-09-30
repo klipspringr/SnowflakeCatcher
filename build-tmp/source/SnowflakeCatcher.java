f14a7e41448d7c9e78bd149ad6d104cde73036ff
unknown <maibarra@697-334-Gimli.sf.k12.internal>
2014-09-30 10:08:12 -0700

    added program

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
 SnowFlake [] billy;
-void setup()
+public void setup()
 {
   size(600,600);
   frameRate(200);
@@ -10,7 +26,7 @@ void setup()
  		billy[i] = new SnowFlake();
  	}
 }
-void draw()
+public void draw()
 {
 
 	
@@ -25,7 +41,7 @@ void draw()
  	}
 
 }
-void mouseDragged()
+public void mouseDragged()
 {
 	
 	if (mouseButton == RIGHT)
@@ -57,7 +73,7 @@ class SnowFlake
     isMoving = true;
     opacity = 255;
   }
-  void show()
+  public void show()
   {
 
     fill(255,opacity);
@@ -70,7 +86,7 @@ class SnowFlake
 	line(x - 4, y +4, x+4, y-4);
 	stroke(0);
 }
-  void lookDown()
+  public void lookDown()
   {
     if (get(x, y+7) != color(0)&& y > 0 && y < 593)
     {
@@ -80,12 +96,12 @@ class SnowFlake
     	isMoving = true;
     }
   }
-  void base()
+  public void base()
   {
     fill(0);
     ellipse(x, y, 12, 12);
   }
-  void move()
+  public void move()
   {
     if (isMoving == true)
     {
@@ -99,7 +115,7 @@ class SnowFlake
 	
     }
   }
-  void wrap()
+  public void wrap()
   {
     if(y >= 600)
     {
@@ -111,3 +127,12 @@ class SnowFlake
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
